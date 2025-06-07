5e5b7667da7aaa63d094e4db3d4e90661fcb7cf8
ArthurHakobyanDev <arthurhakobyan1@gmail.com>
2025-06-07 12:39:20 -0700

    updated buttons on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,13 @@ import './Header.css';
 const Header = () => {
   return (
     <div className="header">
-      <div className="logo">VERCA</div>
+      <div className="left">
+        <div className="logo">VERCA</div>
+      </div>
+      <div className="right">
+        <button className="header-button">Product Policy</button>
+        <button className="header-button">Contact Us</button>
+      </div>
     </div>
   );
 };
