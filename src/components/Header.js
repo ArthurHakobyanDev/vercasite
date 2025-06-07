6e509c30b272d95082cef1243870d0d059dadcd1
ArthurHakobyanDev <arthurhakobyan1@gmail.com>
2025-06-07 12:41:17 -0700

    Verca logo placement

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,10 @@ import './Header.css';
 const Header = () => {
   return (
     <div className="header">
-      <div className="left">
-        <div className="logo">VERCA</div>
-      </div>
-      <div className="right">
-        <button className="header-button">Product Policy</button>
+      <div className="logo">VERCA</div>
+      <div className="header-buttons">
         <button className="header-button">Contact Us</button>
+        <button className="header-button">Product Policy</button>
       </div>
     </div>
   );
